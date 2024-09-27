e0776f69fd9ca56b34066d9b5db05a9a8623757f
KIRUTHIK M R <kiruthikpurpose@gmail.com>
2024-09-27 07:43:32 +0530

    Pushed images from mainspace to images folder

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function rollDice() {
     const dice = document.getElementById('dice');
     const result = document.getElementById('result');
-    const diceImages = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+    const diceImages = ["images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png"];
     
     result.textContent = "Rolling the dice...";
     
